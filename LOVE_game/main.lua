e0ec8b0564f377fcf2548ef26dfb23bf583a5726
Tan arin <tanarin12@gmail.com>
2019-03-26 12:35:00 -0400

    Add Variables/Button Movement
    Put in player movement variables like speed, position, and acceleration. Also added arrow key movement but I will most likely change this to WASD key movement next.

--- LOVE_game/main.lua
+++ LOVE_game/main.lua
@@ -1,19 +1,40 @@
 
 
 function love.load()
+
+	--load player spritesheet
 	playerImg = love.graphics.newImage("assets/sheet_hero_idle.png")
-	playerAnimation = newAnimation(playerImg, 64, 64, 0.5)
+	playerAnimation = newAnimation(playerImg, 64, 64, 0.7)
 	love.graphics.setNewFont(12)
 	love.graphics.setColor(255,255,255)
-	--love.graphics.setBackgroundColor(255,255,255)
-	num = 0
+	love.graphics.setBackgroundColor(255,255,255)
+	
+	--Variables
+	playerX = 50
+	
+	playerSpeed = 0
+	playerSpeedMax = 200
+	playerAcceleration = 15
+	
 end
 
 function love.update(dt)
-	if love.keyboard.isDown("down") then
-		num = num + 100 * dt
+	if love.keyboard.isDown("left") then
+		playerX = playerX - playerSpeed * dt
+		playerSpeed = playerSpeed + playerAcceleration
+	end
+	if love.keyboard.isDown("left") == false then
+		playerSpeed = 0
+	end
+	if love.keyboard.isDown("right") then
+		playerX = playerX + playerSpeed * dt
+		playerSpeed = playerSpeed + playerAcceleration
+	end
+	if love.keyboard.isDown("right") == false then
+		playerSpeed = 0
 	end
 	
+	--Moving through spritesheet here
 	playerAnimation.currentTime = playerAnimation.currentTime + love.timer.getDelta()
 	if playerAnimation.currentTime >= playerAnimation.duration then
 		playerAnimation.currentTime = playerAnimation.currentTime - playerAnimation.duration
@@ -23,12 +44,10 @@ function love.update(dt)
 end
 
 function love.draw()
-		local spriteNum =  math.floor(playerAnimation.currentTime / playerAnimation.duration * #playerAnimation.quads) + 1
-		love.graphics.draw(playerAnimation.spriteSheet, playerAnimation.quads[spriteNum], 0, 0, 0, 4)
-	
 
-	love.graphics.print("Click and drag the eyes", 100, 100)
-	love.graphics.print("whaT", 200, num)
+	local spriteNum =  math.floor(playerAnimation.currentTime / playerAnimation.duration * #playerAnimation.quads) + 1
+	love.graphics.draw(playerAnimation.spriteSheet, playerAnimation.quads[spriteNum], playerX, 210, 0, 4)
+
 end
 
 function love.quit()
