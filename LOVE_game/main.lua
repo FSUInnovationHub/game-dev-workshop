9151f88553e2175d91d071d21313ab4d17c6b356
Tan arin <tanarin12@gmail.com>
2019-03-26 14:50:14 -0400

    Add Background, Friction, WASD
    Added a background and set the window size to be 512 by 256 for now, this could change. Also got the player deceleration to work as well as changed the controls to be WASD

--- LOVE_game/main.lua
+++ LOVE_game/main.lua
@@ -1,38 +1,85 @@
 
+--Lua has tables, much like objects
+player = {}	--This is how a table is created
 
 function love.load()
+
+	--Sets size of the window
+	success = love.window.setMode(512, 256)
 
 	--load player spritesheet
 	playerImg = love.graphics.newImage("assets/sheet_hero_idle.png")
 	playerAnimation = newAnimation(playerImg, 64, 64, 0.7)
-	love.graphics.setNewFont(12)
-	love.graphics.setColor(255,255,255)
-	love.graphics.setBackgroundColor(255,255,255)
+
+	--load background
+	love.graphics.setBackgroundColor(0,230/255,100/255)
 	
-	--Variables
-	playerX = 50
+	background1 = love.graphics.newImage("assets/background/background1.png")
+	background2 = love.graphics.newImage("assets/background/background2.png")
+	background3 = love.graphics.newImage("assets/background/background3.png")
+	background4 = love.graphics.newImage("assets/background/background4.png")
+	background5 = love.graphics.newImage("assets/background/background5.png")
+	background6 = love.graphics.newImage("assets/background/background6.png")
 	
-	playerSpeed = 0
-	playerSpeedMax = 200
-	playerAcceleration = 15
+	-- Player Variables
+	player.x = 50
+	
+	player.speedLeft = 0
+	player.speedRight = 0
+	player.speedMax = 500
+	player.acceleration = 15
+	player.deceleration = 25
+
 	
 end
 
 function love.update(dt)
-	if love.keyboard.isDown("left") then
-		playerX = playerX - playerSpeed * dt
-		playerSpeed = playerSpeed + playerAcceleration
+
+--Keyboard Controls
+	
+	-- A key
+	if love.keyboard.isDown("a") then
+		--Accelerates player until max speed towards left.
+		player.x = player.x - player.speedLeft * dt
+		player.speedLeft = player.speedLeft + player.acceleration
+		
+		if player.speedLeft >= player.speedMax then
+			player.speedLeft = player.speedMax
+		end
+	else
+		--Friction to slow player to a stop
+		player.x = player.x - player.speedLeft * dt
+		player.speedLeft = player.speedLeft - player.deceleration
+		if player.speedLeft <= 0 then
+			player.speedLeft = 0
+		end
 	end
-	if love.keyboard.isDown("left") == false then
-		playerSpeed = 0
+		
+	-- D Key
+	if love.keyboard.isDown("d") then
+		--Accelerates player until max speed towards right.
+		player.x = player.x + player.speedRight * dt
+		player.speedRight = player.speedRight + player.acceleration
+		
+		if player.speedRight >= player.speedMax then
+			player.speedRight = player.speedMax
+		end
+	else
+		--Friction to slow player to a stop
+		player.x = player.x + player.speedRight * dt
+		player.speedRight = player.speedRight - player.deceleration
+		if player.speedRight <= 0 then
+			player.speedRight = 0
+		end
 	end
-	if love.keyboard.isDown("right") then
-		playerX = playerX + playerSpeed * dt
-		playerSpeed = playerSpeed + playerAcceleration
-	end
-	if love.keyboard.isDown("right") == false then
-		playerSpeed = 0
-	end
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	--Moving through spritesheet here
 	playerAnimation.currentTime = playerAnimation.currentTime + love.timer.getDelta()
@@ -45,8 +92,23 @@ end
 
 function love.draw()
 
+	--Draw Background Image
+	love.graphics.draw(background1)
+	love.graphics.draw(background2)
+	love.graphics.draw(background3)
+	love.graphics.draw(background4)
+	love.graphics.draw(background5)
+
+	--floors the number so the spriteSheet goes to the next sprite rather than in between.
 	local spriteNum =  math.floor(playerAnimation.currentTime / playerAnimation.duration * #playerAnimation.quads) + 1
-	love.graphics.draw(playerAnimation.spriteSheet, playerAnimation.quads[spriteNum], playerX, 210, 0, 4)
+	
+	--Draw Animation: love.graphics.draw( texture, quad, x, y, orientation, scale x, scale y, offset x, offset y, Shearing x, Shearing y )
+	--https://www.love2d.org/wiki/love.graphics.draw
+	love.graphics.draw(playerAnimation.spriteSheet, playerAnimation.quads[spriteNum], player.x, 176, 0, 1)
+	
+	
+	--final background image to be in front of the player
+	love.graphics.draw(background6)
 
 end
 
